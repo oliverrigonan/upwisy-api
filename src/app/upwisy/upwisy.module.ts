2cf1234d9d4e88fe534a7a703e92ed4ffca53e6f
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-12-04 14:01:11 +0800

    fix: removed and retain 1 gateway

--- src/app/upwisy/upwisy.module.ts
+++ src/app/upwisy/upwisy.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { CourseGeneratorModule } from './course-generator/course-generator.module';
-import { AssessmentGeneratorModule } from './assessment-generator/assessment-generator.module';
 
 @Module({
   imports: [
     CourseGeneratorModule,
-    AssessmentGeneratorModule
   ],
 })
 export class UpwisyModule { }
