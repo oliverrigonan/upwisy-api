5721f98af8bf1fb3b584272d1eab0212d4391aac
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-12-11 20:58:46 +0800

    deployment: changed the mongodb url

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,6 +3,6 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
-        useFactory: (): Promise<typeof mongoose> => mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/upwisy'),
+        useFactory: (): Promise<typeof mongoose> => mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/upwisy'),
     },
 ];
